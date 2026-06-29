644d35636f840c77b8c28f9d3766f8b9152577da
shiyiran <shiyr21@mails.tsinghua.edu.cn>
2026-06-29 22:58:07 +0800

    fix: bug fix

--- src/openpi/training/data_loader.py
+++ src/openpi/training/data_loader.py
@@ -185,7 +185,7 @@ def transform_dataset(dataset: Dataset, data_config: _config.DataConfig, *, skip
         [
             *data_config.repack_transforms.inputs,
             *data_config.data_transforms.inputs,
-            _transforms.Normalize(norm_stats, use_quantiles=data_config.use_quantile_norm),
+            _transforms.Normalize(norm_stats, use_quantiles=data_config.use_quantile_norm, use_linear_quantile=data_config.use_linear_quantile_norm),
             *data_config.model_transforms.inputs,
         ],
     )
@@ -213,7 +213,7 @@ def transform_iterable_dataset(
         [
             *data_config.repack_transforms.inputs,
             *data_config.data_transforms.inputs,
-            _transforms.Normalize(norm_stats, use_quantiles=data_config.use_quantile_norm),
+            _transforms.Normalize(norm_stats, use_quantiles=data_config.use_quantile_norm, use_linear_quantile=data_config.use_linear_quantile_norm),
             *data_config.model_transforms.inputs,
         ],
         is_batched=is_batched,
